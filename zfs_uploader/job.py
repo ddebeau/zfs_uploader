90bb219ba077d59ca31f17cf21e24407364f2dd1
David Debeau <ddebeau@gmail.com>
2023-01-21 16:09:49 -0600

    Destroy file system if there are no snapshots

--- zfs_uploader/job.py
+++ zfs_uploader/job.py
@@ -7,7 +7,7 @@ from boto3.s3.transfer import TransferConfig
 
 from zfs_uploader.backup_db import BackupDB
 from zfs_uploader.snapshot_db import SnapshotDB
-from zfs_uploader.zfs import (get_snapshot_send_size,
+from zfs_uploader.zfs import (destroy_filesystem, get_snapshot_send_size,
                               get_snapshot_send_size_inc,
                               open_snapshot_stream,
                               open_snapshot_stream_inc, rollback_filesystem,
@@ -250,6 +250,12 @@ class ZFSjob:
         backup_time = backup.backup_time
         backup_type = backup.backup_type
         s3_key = backup.s3_key
+
+        if not snapshots:
+            # Since we can't use the `-F` with `zfs receive` for encrypted
+            # file systems we have to remove file systems before restoring
+            # ourselves.
+            destroy_filesystem(backup.filesystem)
 
         if backup_type == 'full':
             if backup_time in snapshots and filesystem is None:
